b3aee014719744e9837e77a6c14ee99b62ff3c15
Raj <raj819314@gmail.com>
2024-07-04 23:28:32 +0530

    Add breadcrumb

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,5 +1,19 @@
+import { Breadcrumb } from 'antd';
+import { Link } from 'react-router-dom';
+
 const Users = () => {
-    return <h1>Users</h1>;
+    return (
+        <div>
+            <Breadcrumb
+                items={[
+                    {
+                        title: <Link to={'/'}>Dashboard</Link>,
+                    },
+                    { title: 'Users' },
+                ]}
+            />
+        </div>
+    );
 };
 
 export default Users;
