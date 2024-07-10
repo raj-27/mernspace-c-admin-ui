4a0adad1b8f4671420aecc39d1e29935c5af654b
Raj <raj819314@gmail.com>
2024-07-10 15:38:25 +0530

    change time debouncing function time  to 500 miliseconds

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -88,7 +88,7 @@ const Users = () => {
     const debouncedQUpdate = useMemo(() => {
         return debounce((value: string | undefined) => {
             setQueryParams((prev) => ({ ...prev, q: value }));
-        }, 1000);
+        }, 500);
     }, []);
 
     const {
