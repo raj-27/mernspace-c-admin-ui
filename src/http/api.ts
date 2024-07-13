894c6e2d35b1ba23839b9ff4983d3d0155e264e0
Raj <raj819314@gmail.com>
2024-07-14 00:27:45 +0530

    Refactor and add api endpoint for updating user and tenant

--- src/http/api.ts
+++ src/http/api.ts
@@ -1,14 +1,24 @@
-import { Credentials, Tenant, User } from '../types';
+import { CreateUserData, Credentials, Tenant, User } from '../types';
 import { api } from './client';
 
 export const login = (credential: Credentials) =>
     api.post('/auth/login', credential);
 
+// Users Operation
 export const self = () => api.get('/auth/self');
 export const logout = () => api.post('/auth/logout');
 export const getUsers = (queryString: string) =>
     api.get(`/users?${queryString}`);
+export const createUser = (user: User) => api.post('/users', user);
+export const updateUser = (user: CreateUserData, id: number) =>
+    api.patch(`users/${id}`, user);
+
+// Tenants Operation
 export const getTenants = (queryString: string) =>
     api.get(`/tenants?${queryString}`);
-export const createUser = (user: User) => api.post('/users', user);
+
 export const createTenant = (tenant: Tenant) => api.post('/tenants', tenant);
+
+export const updateTenant = (tenant: Tenant, id: number) => {
+    return api.patch(`/tenants/${id}`, tenant);
+};
